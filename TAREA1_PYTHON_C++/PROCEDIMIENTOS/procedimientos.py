7cc4cfaf5557017748ad9a228f6e1d4c7e23cab7
Fidel-DevDyscovery <162225999+Fidel-DevDyscovery@users.noreply.github.com>
2024-04-12 12:02:57 +1200

    Add files via upload

---
+++ TAREA1_PYTHON_C++/PROCEDIMIENTOS/procedimientos.py
@@ -0,0 +1,15 @@
+def sumar(numero1,numero2):
+    resultado=numero1+numero2
+    print("El resultado de la suma es: ",resultado)
+
+def saludo(nombre):
+    print("Hola mi nombre es: ",nombre)
+
+if __name__=="__main__":
+    numero1=10
+    numero2=20
+
+    sumar(numero1,numero2)
+
+    nombre ="Fidel"
+    saludo(nombre)
