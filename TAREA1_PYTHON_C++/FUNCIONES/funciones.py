4b55a5b818c46dcd79d18c2d5150abdbf142041e
Fidel-DevDyscovery <162225999+Fidel-DevDyscovery@users.noreply.github.com>
2024-04-12 12:04:04 +1200

    Add files via upload

---
+++ TAREA1_PYTHON_C++/FUNCIONES/funciones.py
@@ -0,0 +1,18 @@
+def sumar(numero1,numero2):
+    return numero1+numero2
+
+if __name__=="__main__":
+    numero1 =10
+    numero2 =20
+
+    suma1=0
+    suma2=0
+    suma3=0
+
+    suma=sumar(numero1,numero2)
+    suma2=sumar(10,10)
+    suma3=sumar(1,10)
+
+    print("el resultado de la primera suma es: ",suma)
+    print("el resultado de la segunda suma es: ",suma2)
+    print("el resultado de la tercera suma es: ",suma3)
