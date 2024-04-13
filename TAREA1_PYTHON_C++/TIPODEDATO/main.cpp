8ad9375281fb93348d01a735013734b72b289ece
Fidel-DevDyscovery <162225999+Fidel-DevDyscovery@users.noreply.github.com>
2024-04-14 03:47:49 +1200

    Add files via upload

---
+++ TAREA1_PYTHON_C++/TIPODEDATO/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero = 69;
+    double numero2 = 85.25;
+    string nombre = "Fidel";
+    char letra = 'S';
+    bool resultado = true;
+
+    cout << "Mi nombre es: " << nombre << endl;
+
+    return 0;
+}
