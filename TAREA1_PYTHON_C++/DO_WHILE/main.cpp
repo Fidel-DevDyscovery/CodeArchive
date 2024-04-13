e6778e01f199196971cf0d22241a99152548d735
Fidel-DevDyscovery <162225999+Fidel-DevDyscovery@users.noreply.github.com>
2024-04-14 03:45:21 +1200

    Add files via upload

---
+++ TAREA1_PYTHON_C++/DO_WHILE/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+
+    do{
+        cout << "el contador es:" << x << endl;
+        x++;
+    }while(x<10);
+    return 0;
+}
